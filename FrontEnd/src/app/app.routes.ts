30a9d0e1ecb45fe307f5e3dc02f46defe0c176fe
tsolya <tardosjuliannaorsolya@turr.hu>
2026-02-21 17:12:48 +0100

    FLAV-16 Dizájn hibák korrigálása

--- FrontEnd/src/app/app.routes.ts
+++ FrontEnd/src/app/app.routes.ts
@@ -1,6 +1,6 @@
 import { Routes } from '@angular/router';
-import { LoginComponent } from './components/common/login/login.component';
 import { HomeComponent } from './components/common/home/home.component';
+import { LoginComponent } from './components/common/login/login.component';
 
 export const routes: Routes = [
   { path: 'login', component: LoginComponent },
