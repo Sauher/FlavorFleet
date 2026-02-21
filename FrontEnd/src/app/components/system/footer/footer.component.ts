57464645a2d6c9767c94095bbef4c08f5e7e7a11
Schauer Olivér <schaueroliver@turr.hu>
2026-02-21 15:59:12 +0100

    FLAV-10 Footer létrehozása és megvalósítása

--- FrontEnd/src/app/components/system/footer/footer.component.ts
+++ FrontEnd/src/app/components/system/footer/footer.component.ts
@@ -1,12 +1,14 @@
 import { Component } from '@angular/core';
+import { RouterModule } from '@angular/router';
+import { DividerModule } from 'primeng/divider';
 
 @Component({
   selector: 'app-footer',
   standalone: true,
-  imports: [],
+  imports: [RouterModule, DividerModule],
   templateUrl: './footer.component.html',
   styleUrl: './footer.component.scss'
 })
 export class FooterComponent {
-
+  currentYear = new Date().getFullYear();
 }
