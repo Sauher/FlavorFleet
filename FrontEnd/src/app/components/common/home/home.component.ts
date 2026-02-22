bbfb5ba3bf4df448b9a7bcde17d01a5007a5b68e
Schauer Olivér <schaueroliver@turr.hu>
2026-02-22 15:15:39 +0100

    FLAV-10 Footer rendezése

--- FrontEnd/src/app/components/common/home/home.component.ts
+++ FrontEnd/src/app/components/common/home/home.component.ts
@@ -1,7 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
-
 import { CardModule } from 'primeng/card';
 import { ButtonModule } from 'primeng/button';
 import { InputTextModule } from 'primeng/inputtext';
